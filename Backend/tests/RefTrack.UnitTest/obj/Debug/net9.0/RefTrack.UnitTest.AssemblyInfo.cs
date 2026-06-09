aaa105a9095e77fc9ce780e77b0933526d9b3d85
A11278 <A11278@PC-PXET-O-0084>
2026-06-09 15:44:17 +0530

    FILE STRUCTURE WITH CODE

--- Backend/tests/RefTrack.UnitTest/obj/Debug/net9.0/RefTrack.UnitTest.AssemblyInfo.cs
+++ Backend/tests/RefTrack.UnitTest/obj/Debug/net9.0/RefTrack.UnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RefTrack.UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d447da3d92cac4756ccd94730220919d03607e23")]
 [assembly: System.Reflection.AssemblyProductAttribute("RefTrack.UnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RefTrack.UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
